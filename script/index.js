81ac7b5f9e7e7e883b8757c4e1c65699a5c98f14
Yotsaphon Liupolvanish <yotsaphon.liu@thinkerfint.com>
2024-11-22 02:59:10 +0700

    testimonial

--- script/index.js
+++ script/index.js
@@ -22,3 +22,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+let items = document.querySelectorAll(".testimonials-list .testimonial-item");
+let next = document.getElementById("right-arrow");
+let prev = document.getElementById("left-arrow");
+
+// let active = 1;
+// function loadShowSlide() {
+//   let stt = 0;
+
+//   items[active].style.transform = `none`;
+//   items[active].style.zIndex = 10;
+
+//   for (var i = active + 1; i < items.length; i++) {
+//     stt++;
+//     items[i].style.transform = `translateX(${(606 + 30) * stt}px)`;
+//     items[i].style.zIndex = 1;
+//   }
+
+//   stt = 0;
+//   for (var i = active - 1; i >= 0; i--) {
+//     items[i].style.transform = `translateX(${-(606 + 30) * stt}px)`;
+//     items[i].style.zIndex = 1;
+//     stt++;
+//   }
+// }
+// loadShowSlide();
+
+// next.onclick = function () {
+//   active = active + 1 < items.length ? active + 1 : active;
+//   loadShowSlide();
+// };
+
+// prev.onclick = function () {
+//   active = active - 1 >= 0 ? active - 1 : active;
+//   console.log(`${active}`);
+//   loadShowSlide();
+// };
